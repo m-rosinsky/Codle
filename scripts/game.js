dfdd1562d369ba1d491ea08a82f2ecd69cf424a1
m-rosinsky <michael.rosinsky@uconn.edu>
2024-03-15 09:53:09 +0000

    Added ace highlighting

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,6 @@
-/*
- * This function gets a random problem string.
- */
-function get_string() {
-    const sentence = "The quick brown ~ jumps over the lazy ~";
-
-    // Split the sentence.
-    const words = sentence.split(' ');
-
-    let html_string = '';
-
-    words.forEach(word => {
-        if (word.includes('~')) {
-            html_string += '<input type="text" placeholder="${placeholder}" />'
-        } else {
-            html_string += word + ' ';
-        }
-    });
-
-    return html_string.trim();
-}
-
-function highlight_code(code) {
-    return Prism.highlight(code, Prism.languages.python, 'python');
-}
-
 window.onload = function() {
-    const problem_code = get_string();
-    problem_code = highlight_code(problem_code);
+    const problem_code = "print('hello world!')"
     const content_div = document.getElementById("code-editor");
-    content_div.innerHTML = problem_code;
+
+    content_div.innerText = problem_code;
 }
